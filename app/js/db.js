70560320ff348ed993c0407c994dd0856621f0db
Pilivaz <158507610+Pilivaz@users.noreply.github.com>
2026-08-11 09:43:48 -0300

    Add Supabase client initialization in db.js

--- app/js/db.js
+++ app/js/db.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient('https://bbgtqemkyjflglcqzstt.supabase.co', 'sb_publishable_GZzw0LQEA97gcTWhgTx2Zg__jYNtLPS')
+
+
 
